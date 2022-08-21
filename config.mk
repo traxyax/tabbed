9b64fa2a334b6c8e16142de47d4f1c24dfd17842
Aboubacar TRAORE <traxyax@gmail.com>
2022-08-21 22:09:09 +0000

    modifié :         config.mk

--- config.mk
+++ config.mk
@@ -11,14 +11,14 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # freetype
-FREETYPELIBS = -lfontconfig -lXft -lXrender
+FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
 
 # includes and libs
 INCS = -I. -I/usr/include -I$(X11INC) -I${FREETYPEINC}
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${FREETYPELIBS}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${FREETYPELIBS} -lXrender
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE
